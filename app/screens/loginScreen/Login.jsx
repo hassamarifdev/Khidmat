7da1243cbf19898e7a6a8c38aba03f724cca0d85
Hassam Arif <arifhassam18@gmail.com>
2024-01-04 15:14:23 -0800

    first commit

---
+++ app/screens/loginScreen/Login.jsx
@@ -0,0 +1,60 @@
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
+import React from "react";
+import Colors from "../../utils/Colors";
+
+export default function Login() {
+  return (
+    <View style={styles.subContainer}>
+      <Text style={styles.heading}>
+        Let's Find{" "}
+        <Text style={styles.boldHeading}>Professional Cleaning and Repair</Text>{" "}
+        Services
+      </Text>
+      <Text style={styles.text}>Best App to find services near you which deliver you a professional service</Text>
+
+      <TouchableOpacity style={styles.button} onPress={()=>console.log("Button Pressed!")}>
+            <Text style={styles.buttonText}>Let's Get Started</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  subContainer: {
+    position: "absolute",
+
+    width: "100%",
+    height: "35%",
+    backgroundColor: Colors.Primary,
+    bottom: 0,
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    padding: 20,
+  },
+  heading: {
+    color: Colors.White,
+    fontSize: 23,
+    textAlign: "center",
+  },
+  boldHeading: {
+    fontWeight: "bold",
+  },
+  text: {
+    color: Colors.White,
+    marginTop: 20,
+    textAlign: 'center',
+    fontSize: 15
+  },
+  button: {
+    padding: 15,
+    backgroundColor: Colors.White,
+    borderRadius: 99,
+    marginTop: 35,
+  },
+  buttonText: {
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: Colors.Primary,
+  },
+});
---
+++ app/utils/Colors.js
@@ -0,0 +1,5 @@
+export default{
+    Black: "#000000",
+    Primary: "#8E3FFF",
+    White: "#ffffff",
+}

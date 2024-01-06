12abee303c0ec8f3ec4c2f175b2dbf8a67eb551d
Hassam Arif <arifhassam18@gmail.com>
2024-01-05 17:09:40 -0800

    profile section added to the header

--- app/screens/homeScreen/Header.jsx
+++ app/screens/homeScreen/Header.jsx
@@ -1,12 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useUser } from "@clerk/clerk-expo";
+import Colors from "../../utils/Colors";
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function Header() {
+  const { user, isLoading } = useUser();
   return (
-    <View>
-      <Text>Header</Text>
-    </View>
-  )
+    user && (
+      <View style={styles.container}>
+        {/* Profile Section */}
+        <View style={styles.profileMainContainer}>
+        <View style={styles.profileContainer}>
+          <Image source={{ uri: user?.imageUrl }} style={styles.userImage} />
+          <View>
+            <Text style={styles.welcome}>Welcome,</Text>
+            <Text style={styles.text}>{user?.fullName}</Text>
+          </View>
+        </View>
+        <FontAwesome name="bookmark-o" size={27} color="white" />
+      </View>
+      </View>
+    )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingTop: 40,
+    backgroundColor: Colors.Primary,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  profileMainContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: 'space-between',
+  },
+  profileContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  userImage: {
+    width: 45,
+    height: 45,
+    borderRadius: 22.5,
+  },
+  text: {
+    color: Colors.White,
+    fontSize: 20,
+  },
+  welcome: {
+    color: Colors.White,
+  },
+});
